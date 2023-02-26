41773d74449d81e33ae04b503d757edb27974094
spravce <spravce@192.168.0.206>
2023-02-26 10:31:44 +0100

    První commit

---
+++ testovani/src/main/java/com/mycompany/testovani/Testovani.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testovani;
+
+/**
+ *
+ * @author spravce
+ */
+public class Testovani {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World and Good Bye, you fuckers!");
+    }
+}
